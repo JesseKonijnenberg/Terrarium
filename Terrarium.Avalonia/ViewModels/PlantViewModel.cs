0baaba099e19921d1889153022fc37c5e3f45810
Jesse Konijnenberg <jesse.konijnenberg@gmail.com>
2025-12-14 21:58:59 +0100

    Update to 1.0.2

--- Terrarium.Avalonia/ViewModels/PlantViewModel.cs
+++ Terrarium.Avalonia/ViewModels/PlantViewModel.cs
@@ -38,7 +38,7 @@ namespace Terrarium.Avalonia.ViewModels
             _weatherService = weatherService;
             _growthService = growthService;
 
-            _myPlant = new Plant { Name = "Sprout v1.0", Hydration = 50, Sunlight = 50, State = PlantState.Happy };
+            _myPlant = new Plant { Name = "Sprout v1.0.2", Hydration = 50, Sunlight = 50, State = PlantState.Happy };
 
             WaterCommand = new RelayCommand(() => WaterPlant());
 
