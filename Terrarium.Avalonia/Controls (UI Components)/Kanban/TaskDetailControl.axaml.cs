182bb00f475e16e65bf73b7a2ce69cf6ddec4fcb
Jesse K <jesse.konijnenberg@gmail.com>
2026-01-11 23:08:00 +0100

    Updated KanbanBoardView.axaml, split to controls

---
+++ Terrarium.Avalonia/Controls (UI Components)/Kanban/TaskDetailControl.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Terrarium.Avalonia.Controls;
+
+public partial class TaskDetailControl : UserControl
+{
+    public TaskDetailControl()
+    {
+        InitializeComponent();
+    }
+    
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
