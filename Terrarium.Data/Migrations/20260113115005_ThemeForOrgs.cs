c21dcc3b7f797788ece47fe5b71189174568a1ac
Jesse K <jesse.konijnenberg@gmail.com>
2026-01-13 15:44:00 +0100

    Seeder updated

---
+++ Terrarium.Data/Migrations/20260113115005_ThemeForOrgs.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Terrarium.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThemeForOrgs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ActiveThemeId",
+                table: "Organizations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LockTheme",
+                table: "Organizations",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActiveThemeId",
+                table: "Organizations");
+
+            migrationBuilder.DropColumn(
+                name: "LockTheme",
+                table: "Organizations");
+        }
+    }
+}
